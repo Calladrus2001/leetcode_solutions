67eb1d01ba00c1744a4d61462a6be11898993b32
viz.dugs <79591529+Calladrus2001@users.noreply.github.com>
2022-09-25 14:23:00 +0530

    Time: 3 ms (36.00%), Space: 6.3 MB (16.00%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int preClimbStairs(int n,vector<int> &dp){
+             if(n <= 1) return dp[n] = 1;
+             if(dp[n] != -1) return dp[n];
+             int left = preClimbStairs(n-1,dp);
+             int right = preClimbStairs(n-2,dp);
+             return dp[n] = left + right;
+         }
+         int climbStairs(int n) {
+             vector<int> dp(n+1,-1);
+             preClimbStairs(n,dp);
+             return dp[n];
+         }
+};
